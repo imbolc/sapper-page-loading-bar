ab5ceb0ad11b9d40cdfbc4d64ad3872981a819b5
imbolc <imbolc@imbolc.name>
2019-08-19 15:07:54 +0700

    The component

---
+++ PageLoadingBar.svelte
@@ -0,0 +1,106 @@
+{#if $preloading}
+    <div class="progress" {style}>
+        <div class="indeterminate" style={inner_style}></div>
+    </div>
+{/if}
+
+<script>
+    export let preloading
+    export let height = "4px"
+    export let color1 = "#acece6"
+    export let color2 = "#26a69a"
+
+    $: style = `background-color: ${color1}; height: ${height}`
+    $: inner_style = `background-color: ${color2}`
+</script>
+
+<style>
+    .progress {
+        position: fixed;
+        top: 0;
+        z-index: 1000;
+        height: 4px;
+        width: 100%;
+        border-radius: 2px;
+        background-clip: padding-box;
+        overflow: hidden;
+    }
+    .progress .indeterminate:before {
+        content: '';
+        position: absolute;
+        background-color: inherit;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        will-change: left, right;
+        -webkit-animation: indeterminate 2.1s cubic-bezier(0.65, 0.815, 0.735, 0.395) infinite;
+        animation: indeterminate 2.1s cubic-bezier(0.65, 0.815, 0.735, 0.395) infinite;
+    }
+    .progress .indeterminate:after {
+        content: '';
+        position: absolute;
+        background-color: inherit;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        will-change: left, right;
+        -webkit-animation: indeterminate-short 2.1s cubic-bezier(0.165, 0.84, 0.44, 1) infinite;
+        animation: indeterminate-short 2.1s cubic-bezier(0.165, 0.84, 0.44, 1) infinite;
+        -webkit-animation-delay: 1.15s;
+        animation-delay: 1.15s;
+    }
+    @-webkit-keyframes indeterminate {
+        0% {
+            left: -35%;
+            right: 100%;
+        }
+        60% {
+            left: 100%;
+            right: -90%;
+        }
+        100% {
+            left: 100%;
+            right: -90%;
+        }
+    }
+    @keyframes indeterminate {
+        0% {
+            left: -35%;
+            right: 100%;
+        }
+        60% {
+            left: 100%;
+            right: -90%;
+        }
+        100% {
+            left: 100%;
+            right: -90%; } }
+    @-webkit-keyframes indeterminate-short {
+        0% {
+            left: -200%;
+            right: 100%;
+        }
+        60% {
+            left: 107%;
+            right: -8%;
+        }
+        100% {
+            left: 107%;
+            right: -8%;
+        }
+    }
+    @keyframes indeterminate-short {
+        0% {
+            left: -200%;
+            right: 100%;
+        }
+        60% {
+            left: 107%;
+            right: -8%;
+        }
+        100% {
+            left: 107%;
+            right: -8%;
+        }
+    }
+</style>
--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# sapper-page-loading-bar
-Page preloader for Sapper
+# Page preloader for Sapper
+
+I believe this component should be included into [sapper-template][]
+by default as due to it's AJAX nature page loading looks a bit broken now.
+
+Try to enable "Slow 3G" throttling in Chrome DevTools and follow
+a link on a [sapper][]-based site. Choose a link without [prefetch][] attribute
+to see the strongest effect. Did it felt broken enough that you've clicked
+the link twice or tried another one? :)
+
+To see what exactly went wrong you can disable js and try to follow the same link again.
+You'll see:
+
+- toolbar reloading icon turns into a cross
+- tab favicon turns into a spinner
+- page url will change only after the page loading
+
+Nothing of these happens with enabled js, but instant url change is
+probably the most noticeable effect.
+
+A usual way of fixing this is adding some kind of loading indicator.
+And here's how to add a top bar - probably the most common type of these
+indicators natively existing in mobile browsers.
+
+# Setup
+
+Install the component:
+
+```sh
+npm i sapper-page-loading-bar
+```
+
+and include it at the top of your `src/routes/_layout.svelte`,
+you can also add a transition effect on page loading:
+
+```html
+<PageLoadingBar {preloading}/>
+
+{#if !$preloading}
+    <main transition:fade>
+        <slot></slot>
+    </main>
+{/if}
+
+<script>
+    import { fade } from "svelte/transition"
+    import { stores } from "@sapper/app"
+    import PageLoadingBar from "sapper-page-loading-bar/PageLoadingBar.svelte"
+
+    const { preloading } = stores()
+</script>
+```
+
+## Tuning
+
+While `preloading` parameter is required, there's also a few optional of them you
+can use to shape the preloader appearance:
+
+- `height` - height of the preloader line
+- `color1` and `color2` - its colors
+
+
+[sapper]: https://sapper.svelte.dev/
+[sapper-template]: https://github.com/sveltejs/sapper-template
+[prefetch]: https://sapper.svelte.dev/docs#prefetch_href
